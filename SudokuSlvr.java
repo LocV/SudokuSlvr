79b4ac9e1b172e2330f9930ce600453939100b0d
LocV <loc@vuudoo.com>
2017-02-21 08:45:48 -0800

    Print the grid.

---
+++ SudokuSlvr.java
@@ -0,0 +1,40 @@
+public class SudokuSlvr {
+
+    public static void main(String[] args) {
+        System.out.println("-------------");
+        System.out.println("|   |   |   |");
+        System.out.println("| - | - | - |");
+        System.out.println("|   |   |   |");
+
+        System.out.println("-------------");
+        System.out.println("|   |   |   |");
+        System.out.println("| - | - | - |");
+        System.out.println("|   |   |   |");
+
+        System.out.println("-------------");
+        System.out.println("|   |   |   |");
+        System.out.println("| - | - | - |");
+        System.out.println("|   |   |   |");
+        System.out.println("-------------");
+
+        System.out.print("\f");
+
+        System.out.println("-------------");
+        System.out.println("|   |   |   |");
+        System.out.println("| - | - | - |");
+        System.out.println("|   |   |   |");
+
+        System.out.println("-------------");
+        System.out.println("|   |   |   |");
+        System.out.println("| - | - | - |");
+        System.out.println("|   |   |   |");
+
+        System.out.println("-------------");
+        System.out.println("|   |   |   |");
+        System.out.println("| - | - | - |");
+        System.out.println("|   |   |   |");
+        System.out.println("-------------");
+
+    }
+
+}
